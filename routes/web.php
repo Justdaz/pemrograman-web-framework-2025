5a137e572a0d2c2cc7044cc765013b28f3bedcc8
Helix <raffida2013@gmail.com>
2025-08-29 08:26:50 +0700

    update

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,13 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+//Rute GET sederhana
+Route::get('/hello', function(){
+    return 'Hello, World!';
+});
+
+//RUte dengan parameter
+Route::get('/user/{id}', function($id){
+    return "User ID:" . $id
+});
