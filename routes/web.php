c13cd46a29da0e5c2fc85221ae3026bf8b07bcc0
Helix <raffida2013@gmail.com>
2025-09-03 15:26:07 +0700

    update

--- routes/web.php
+++ routes/web.php
@@ -6,12 +6,27 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-//Rute GET sederhana
-Route::get('/hello', function(){
-    return 'Hello, World!';
-});
 
-//RUte dengan parameter
-Route::get('/user/{id}', function($id){
-    return "User ID:" . $id
+//Rute dengan parameter
+// Route::get('/user/{id}', function($id){
+//     return "User ID:" . $id;
+// });
+
+//Rute dengan parameter
+Route::prefix('manage')->group(function(){
+
+Route::get('/user/{name?}', function($name= 'Guest'){
+    return "Hello," . $name;
+})->name('user');
+
+Route::get('/profile', function(){
+    return "This is the profile page.";
+})->name('profile');
+
+//Rute dengan parameter
+Route::get('/redirect-to-profile', function(){
+    return redirect()->route('user');
 });
+}
+
+);
