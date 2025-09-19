1c582c5e0416be620c1458d7589869d5ca6dc3dd
Helix <raffida2013@gmail.com>
2025-09-19 10:43:55 +0700

    pertemuan 4

--- routes/web.php
+++ routes/web.php
@@ -2,19 +2,34 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/rahasia', function(){
+    return 'ini halaman rahasia';
+})->middleware(['auth','RoleCheck:admin']);
+
 Route::get('/dashboard', function () {
     return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+})->middleware(['auth', 'verified','RoleCheck:admin'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+Route::get('/product', [ProductController::class,'index']);
+// Route::get('/product/create', [ProductController::class,'create']);
+// Route::get('/product', [ProductController::class,'store']);
+// Route::get('/product/{id}', [ProductController::class,'show']);
+// Route::get('/product/{id}/edit', [ProductController::class,'edit']);
+// Route::get('/product/{id}', [ProductController::class,'update']);
+// Route::get('/product/{id}', [ProductController::class,'destroy']);
+
+
 
 require __DIR__.'/auth.php';
